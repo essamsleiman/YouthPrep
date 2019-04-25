907ccd93c65104254f7092072ea8a3e0f2b225b9
Suma Sleiman <SumaSleiman@sumas-macbook-air.local>
2019-04-24 20:24:40 -0700

    Search ability

--- YouthPrep/SearchViewController.swift
+++ YouthPrep/SearchViewController.swift
@@ -2,29 +2,15 @@
 //  SearchViewController.swift
 //  YouthPrep
 //
-//  Created by Suma Sleiman on 4/18/19.
+//  Created by Suma Sleiman on 4/24/19.
 //  Copyright © 2019 YouthPrepCo. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class SearchViewController: UIViewController {
-
+class SearchViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
